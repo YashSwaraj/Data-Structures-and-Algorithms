f8edaab327fbe4bf50a53019e41f3f326c1e3c60
YashSwaraj <yash.swaraj@gmail.com>
2019-09-28 00:51:19 +0530

    Removed

--- reverseStacks.c
+++ reverseStacks.c
@@ -3,32 +3,6 @@
 
 int main()
 {
-    int arr[50], tA=-1, tB=50, ch, n;
-
-    printf("1. Push to stack A\n");
-    printf("2. Pop from stack A\n");
-    printf("3. Push to stack B\n");
-    printf("4. Pop from stack B\n");
-    printf("5. Exit\n");
-
-    while(1)
-    {
-        scanf("%d", &ch);
-        switch (ch)
-        {
-        case 1:
-            if (tA < tB-1)
-            {
-                printf("Enter number for pushing");
-                scanf("%d", &n);
-                tA++;
-                arr[tA] = n;
-            }
-            else
-                printf("Overflow");
-            break;
-        case 2:
-            if (tA > -1)
             {
                 printf("%d", arr[tA]);
                 tA--;
